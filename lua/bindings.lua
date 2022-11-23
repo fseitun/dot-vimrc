829c4ab34dcff47085fdac09990cfef2ad625bfd
Federico Seitún <fseitun@gmail.com>
2022-11-23 10:38:49 -0300

    Change bindings to reflect new lua files

--- lua/bindings.lua
+++ lua/bindings.lua
@@ -1,6 +1,11 @@
-vim.keymap.set('n', '<leader>pi', ':PlugInstall<CR>', {noremap = true})
+vim.keymap.set('n', '<leader>pi', ':PackerInstall<CR>', {noremap = true})
 vim.keymap.set('n', '<leader>so', ':source %<CR>', {noremap = true})
 vim.keymap.set('n', '<leader>f', ':Files<CR>', {noremap = true})
 vim.keymap.set('n', '<leader>c', ':Commentary<CR>', {noremap = true})
-vim.keymap.set('n', '<leader>vrc', ':e ~/.config/nvim/init.vim<CR>', {noremap = true})
 vim.keymap.set('n', '<leader>swa', '<Plug>GenerateDiagram', {noremap = true})
+
+vim.keymap.set('n', '<leader>v', ':e ~/.config/nvim/init.lua<CR>', {noremap = true})
+vim.keymap.set('n', '<leader>vb', ':e ~/.config/nvim/lua/bindings.lua<CR>', {noremap = true})
+vim.keymap.set('n', '<leader>vp', ':e ~/.config/nvim/lua/plugins.lua<CR>', {noremap = true})
+vim.keymap.set('n', '<leader>vg', ':e ~/.config/nvim/lua/globals.lua<CR>', {noremap = true})
+
