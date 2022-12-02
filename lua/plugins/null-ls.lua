676c302818dda8540241d70e773850aaa551804b
Federico Seitún <fseitun@gmail.com>
2022-12-02 13:42:14 -0300

    Setup null-ls

--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -1,10 +1,20 @@
 local null_ls = require('null-ls')
 
 null_ls.setup({
+    debug = true,
     sources = {
-        null_ls.builtins.formatting.stylua,
         null_ls.builtins.formatting.prettierd,
-        null_ls.builtins.diagnostics.eslint_d,
-        null_ls.builtins.diagnostics.yamllint,
+        null_ls.builtins.diagnostics.eslint_d.with({
+            extra_filetypes = {},
+            diagnostic_config = {
+                underline = true,
+                virtual_text = true,
+                signs = true,
+                update_in_insert = true,
+                severity_sort = true,
+            },
+        }),
+        -- null_ls.builtins.diagnostics.yamllint,
+        null_ls.builtins.code_actions.eslint_d,
     },
 })
