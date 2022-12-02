4d84edfd3fcd8f2021e4c68a1ea80bb888b782cb
Federico Seitún <fseitun@gmail.com>
2022-12-02 15:50:02 -0300

    Remove debug boolean

--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -1,7 +1,7 @@
 local null_ls = require('null-ls')
 
 null_ls.setup({
-    debug = true,
+    -- debug = true,
     sources = {
         null_ls.builtins.formatting.prettierd,
         null_ls.builtins.diagnostics.eslint_d.with({
