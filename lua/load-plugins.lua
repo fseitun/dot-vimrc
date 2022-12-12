0ad7bb20cd7397c93b0c4f7713f41ef060ade85a
Federico Seitún <fseitun@gmail.com>
2022-12-12 11:25:32 -0300

    Remove trailing space

--- lua/load-plugins.lua
+++ lua/load-plugins.lua
@@ -1,6 +1,6 @@
 return require('packer').startup(function(use)
     use 'wbthomason/packer.nvim'
-    use 'github/copilot.vim' 
+    use 'github/copilot.vim'
     use 'lewis6991/gitsigns.nvim'
     use 'tpope/vim-commentary'
     use 'ap/vim-css-color'
