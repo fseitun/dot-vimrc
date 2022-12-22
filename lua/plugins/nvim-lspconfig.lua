2599e8644784b41378a874832c1de3bdcf98b5c4
Federico Seitún <fseitun@gmail.com>
2022-12-22 14:54:15 -0300

    Add intelephense as LSP for PHP

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -49,3 +49,9 @@ require('lspconfig').cssls.setup{
     flags = lsp_flags,
     capabilities = capabilities,
 }
+
+require('lspconfig').intelephense.setup{
+    on_attach = on_attach,
+    flags = lsp_flags,
+    capabilities = capabilities,
+}
