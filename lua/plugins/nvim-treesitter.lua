2d8c87fee7ebc6794887a37e0753cdb1d96a91fc
Federico Seitún <fseitun@gmail.com>
2023-04-13 09:43:19 -0300

    Add php to treesitter and enable indent

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,5 +1,5 @@
 require'nvim-treesitter.configs'.setup {
-    ensure_installed = {css, html, javascript, json, lua,  typescript, yaml},
+    ensure_installed = {css, html, javascript, json, lua,  typescript, yaml, php},
     highlight = {
         enable = true,
         additional_vim_regex_highlighting = false
@@ -8,5 +8,8 @@ require'nvim-treesitter.configs'.setup {
         enable = true,
         extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
         max_file_lines = nil, -- Do not enable for files with more than n lines, int
+    },
+    indent = {
+        enable = true
     }
 }
