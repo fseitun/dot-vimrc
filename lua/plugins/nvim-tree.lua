d62f5c38c468d254acd0a4e08147bb8104d5880f
Federico Seitún <fseitun@gmail.com>
2023-04-26 10:23:03 -0300

    Change nvim-tree config to use default new suggested config

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -1,26 +1,13 @@
 require("nvim-tree").setup({
   sort_by = "case_sensitive",
-  view = {
-    adaptive_size = true,
-    mappings = {
-      list = {
-        { key = "u", action = "dir_up" },
-      },
-    },
-  },
   renderer = {
     group_empty = true,
   },
   filters = {
     dotfiles = true,
   },
-  update_focused_file = {
-    enable = true,
-    update_root = true,
-  },
 })
 
 require'nvim-web-devicons'.setup {
- default = true,
+  default = true,
 }
-
